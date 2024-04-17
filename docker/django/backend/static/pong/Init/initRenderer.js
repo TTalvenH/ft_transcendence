19b946b8ddcf8ce31ebbd12a22d8cb0a63a0e1ba
eetu koljonen <eetu.koljonen@gmail.com>
2024-04-17 16:13:27 +0300

    pong is on the screen finally

--- docker/django/backend/static/pong/Init/initRenderer.js
+++ docker/django/backend/static/pong/Init/initRenderer.js
@@ -2,10 +2,11 @@ import * as THREE from 'three'
 
 export function initRenderer()
 {
+	const container = document.getElementById("game");
 	const renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.toneMapping = THREE.ReinhardToneMapping;
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
-
+	renderer.setSize(container.clientWidth, container.clientHeight);
+	// document.body.appendChild(renderer.domElement);
+	container.appendChild(renderer.domElement);
 	return renderer;
 }
--- docker/django/backend/static/router.js
+++ docker/django/backend/static/router.js
@@ -1,34 +1,39 @@
 import {Pong} from './pong/pong.js'
 
-
 const route = (event) => {
-	event = event || window.event;
-	event.preventDefault();
-	window.history.pushState({}, "", event.target.href);
-	handleLocation();
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
 };
 
 const routes = {
-	404: "/404",
-	"/": "api/data/home.html",
-	"/login": "api/data/login.html"
+    404: "/404",
+    "/": "api/data/home.html",
+    "/login": "api/data/login.html",
 };
 
 const handleLocation = async () => {
-	const path = window.location.pathname;
-	const route = routes[path] || routes[404];
-	const html = await fetch(route).then((data) => data.text());
-	document.getElementById("main-page").innerHTML = html;
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("game").innerHTML = html;
+    
+    // Add event listener when the page is loaded
+    addEventListenerToPlayButton();
+};
+
+const addEventListenerToPlayButton = () => {
+    const playButton = document.getElementById("playButton");
+    playButton.addEventListener("click", startPongGame);
+};
+
+const startPongGame = () => {
+	document.getElementById("game").innerHTML = ""; //tyhjennetää cardi, tämä on purkkakikka
+    const pongGame = new Pong(); // Assuming Pong() is the constructor for your Pong game
+	pongGame.gameLoop();
 };
 
 window.onpopstate = handleLocation;
 window.route = route;
 handleLocation();
-
-const startGame = () => {
-    // Start your Pong game
-    const pongGame = new Pong(); // Assuming Pong() is the constructor for your Pong game
-    pongGame.gameLoop(); // Call the start method of your Pong game
-};
-
-document.getElementById('startButton').addEventListener('click', startGame);
