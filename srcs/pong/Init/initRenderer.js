288a4f6905d15a81f5cdfd3170aa68ed2b00d16b
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2024-04-18 21:30:56 +0300

    added healthbar, ball bounces now different direction depending where it hits the paddle, also changed init camera angle

--- srcs/pong/Init/initRenderer.js
+++ srcs/pong/Init/initRenderer.js
@@ -2,9 +2,13 @@ import * as THREE from 'three'
 
 export function initRenderer()
 {
+	const container = document.getElementById("neon-container");
+
 	const renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.toneMapping = THREE.ReinhardToneMapping;
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	// renderer.setSize(container.clientWidth, container.clientHeight);
+	// document.getElementById("neon-container").appendChild(renderer.domElement);
 	document.body.appendChild(renderer.domElement);
 
 	return renderer;
