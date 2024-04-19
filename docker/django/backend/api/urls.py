0b75c12be6c391bba02132419c1f86d6d174203e
eetu koljonen <eetu.koljonen@gmail.com>
2024-04-19 13:16:42 +0300

    before merge

--- docker/django/backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'api'
-
-urlpatterns = [
-    path('home.html', views.home, name='home'),
-    path('login.html', views.login, name='login'),
-]
--- docker/django/backend/backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
-
-def error(request):
-    # Assume the login was successful
-    return render(request, '404.html')
